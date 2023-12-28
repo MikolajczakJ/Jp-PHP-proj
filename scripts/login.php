043fea225507d6d7502d46df40a3682154198c0c
Vor Tiu <woj_m@192.168.1.13>
2023-12-28 12:42:22 +0100

    sprawdzenie login

--- scripts/login.php
+++ scripts/login.php
@@ -1,14 +1,46 @@
 <?php
 session_start();
-require_once("./connect.php");
-require_once("./user.php");
-$user = User::findUser($_POST["login-email"],$_POST["login-password"],$conn);
-if($user == null) {
-$_SESSION["error"] = "Błędny login lub hasło";
-}else{
-    // tutaj przypisanie danych użytkownika do sesji
-    User::logInUser($user);
-    header("location: ../strony/index.php");
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["login-email"];
+    $password = $_POST["login-password"];
 
+    // Sprawdzenie czy niepuste dane
+    if (isset($email, $password) && !empty($email) && !empty($password)) {
+        echo "Dane nie puste <br>";
+
+        // Czy poprawny adres email
+        $filteredEmail = filter_var($email, FILTER_VALIDATE_EMAIL);     // Czy email
+        if ($filteredEmail === true) {
+            $trimmedEmail = substr($filteredEmail, 0, 255);     // Do 255 znaków
+            if ($trimmedEmail === true) {
+                $normalizedEmail = strtolower($trimmedEmail);       // wszystko z małej
+
+                $emailParts = explode('@', $normalizedEmail);       // Sprawdzenie domeny
+                $domain = end($emailParts);
+                if (!checkdnsrr($domain, 'MX')) {
+                    echo "Domena niepoprawna";
+                    header("../strony/logandreg.php");
+                } else {
+                    echo "Adres poprawny";
+                    $filterPassword = trim($password);      // Bez białych znaków
+        
+                    require_once("./connect.php");
+                    require_once("./user.php");
+                    $user = User::findUser($normalizedEmail,$filterPassword,$conn);
+                    if($user == null) {
+                        $_SESSION["error"] = "Błędny login lub hasło";
+                    }else{
+                        // tutaj przypisanie danych użytkownika do sesji
+                        User::logInUser($user);
+                        header("location: ../strony/index.php");
+                    }
+                }
+            }
+        } else {
+            echo "Adres niepoprawny";
+        }
+    } else {
+        echo "Dane puste <br>";
+    }
+}
 ?>
